b65e382470a46749fc8ff729248f72d11994d4bf
winton <mail@wintoni.us>
2022-03-29 01:35:37 -0700

    Default engine to FakePostgresRecord

--- src/SequelAST.ts
+++ src/SequelAST.ts
@@ -1,7 +1,8 @@
 import type Engine from './interfaces/Engine.js'
+import { FakePostgresRecord } from './mod.js'
 
-const SequelAST: { engine: Engine | null } = {
-  engine: null,
+const SequelAST: { engine: Engine } = {
+  engine: new FakePostgresRecord()
 }
 
 export default SequelAST
